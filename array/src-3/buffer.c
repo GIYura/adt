7986ee01674a62dad52751c675e680385bcf9078
JHlotov <iglotov@tektelic.com>
2023-08-15 22:00:26 +0300

    add one more approach

---
+++ array/src-3/buffer.c
@@ -0,0 +1,30 @@
+#include "buffer.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+void BufferInit(int* const buffer, int const size)
+{
+    assert(buffer);
+    assert(size > 0);
+
+    for (int i = 0; i < size; i++)
+    {
+        buffer[i] = i;
+    }
+}
+
+void BufferPrint(const int* const buffer, int size)
+{
+    assert(buffer);
+    assert(size > 0);
+
+    printf("Buffer size: %d items\n", size);
+    printf("Buffer: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
+}
+
