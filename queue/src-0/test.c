b61f917d052222d046e494ab10f5a49b38621e1c
JHlotov <iglotov@tektelic.com>
2023-08-17 10:40:27 +0300

    initial commit

---
+++ queue/src-0/test.c
@@ -0,0 +1,10 @@
+/*
+TEST src-0
+*/
+#include "queue.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ stack/src-0/test.c
@@ -0,0 +1,10 @@
+/*
+TEST src-0
+*/
+#include "stack.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
